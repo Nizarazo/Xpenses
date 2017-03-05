7129a79ec3c3a4b1953a2bbca79aad684090d9de
Udi Oron <udioron@gmail.com>
2017-03-05 17:38:14 +0200

    Added more simple views: add. calendar

--- xpenses/urls.py
+++ xpenses/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+import calendar
 import random
 
 from django.conf.urls import url
@@ -32,15 +33,35 @@ def lucky(request):
     })
 
 
-def hello(request, name):  # view function
-    return HttpResponse("Hello <b>{}</b>".format(
-        name.title(),
+def hello(request, name):
+    return HttpResponse("Hello <b>{}</b>".format(name.title()))
+
+
+def add(request, x, y):
+    return HttpResponse("{} + {} = <b>{}</b>".format(
+        x, y, int(x) + int(y)
     ))
+
+
+def calendar_month(request, year, month):
+    # TODO: check month, year
+    return HttpResponse(calendar.HTMLCalendar().formatmonth(
+        int(year), int(month)
+    ))
+
+
+def calendar_year(request, year):
+    s = " ".join('<a href="{0}/">{0}</a>'.format(i)
+                 for i in range(1, 13))
+    return HttpResponse(s)
 
 
 urlpatterns = [
     url(r'^$', home),
     url(r'^api/$', lucky),
     url(r'^hello/([a-zA-Z]+)/$', hello),
+    url(r'^add/([0-9]+)/([0-9]+)/$', add),
+    url(r'^calendar/([0-9]{4})/$', calendar_year),
+    url(r'^calendar/([0-9]{4})/([0-9]{1,2})/$', calendar_month),
     url(r'^admin/', admin.site.urls),
 ]

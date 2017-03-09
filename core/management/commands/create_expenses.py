7ce6cf37a4710d8e950508c3dbb7130a4d103365
Udi Oron <udioron@gmail.com>
2017-03-09 11:49:11 +0200

    Added a simple Expense model and a management command to create silly expenses

---
+++ core/management/commands/create_expenses.py
@@ -0,0 +1,29 @@
+import datetime
+import random
+
+import silly
+from django.core.management.base import BaseCommand
+
+from core.models import Expense
+
+
+class Command(BaseCommand):
+    help = "Creates dummy expenses"
+
+    def add_arguments(self, parser):
+        parser.add_argument('n', type=int)
+
+    def handle(self, *args, **options):
+        for i in range(options['n']):
+            o = Expense(
+                date=datetime.date(
+                    random.randint(2010, 2017),
+                    random.randint(1, 12),
+                    random.randint(1, 28)
+                ),
+                title=silly.a_thing(),
+                amount=random.randint(1, 1000),
+                comments=silly.paragraph(),
+            )
+            o.full_clean()
+            o.save()
--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Expense(models.Model):
+    date = models.DateField()
+    amount = models.DecimalField(decimal_places=2,
+                                 max_digits=10)
+    title = models.CharField(max_length=300)
+    comments = models.TextField(null=True, blank=True)
+

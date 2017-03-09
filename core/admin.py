33460ec5ab4a24224632ab7aade6959a8434d4bb
Udi Oron <udioron@gmail.com>
2017-03-09 12:37:15 +0200

    Added admin customization

--- core/admin.py
+++ core/admin.py
@@ -2,4 +2,21 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.Expense)
+
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'date',
+        'amount',
+        'title',
+    )
+    search_fields = (
+        'id',
+        'date',
+        'amount',
+        'title',
+    )
+    date_hierarchy = "date"
+
+
+admin.site.register(models.Expense, ExpenseAdmin)

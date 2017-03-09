e6624d440dc0e098ad257a856913e1c5b5a71f6a
Udi Oron <udioron@gmail.com>
2017-03-09 16:44:33 +0200

    Add names for urls

--- core/urls.py
+++ core/urls.py
@@ -2,12 +2,15 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = "expenses"
+
 urlpatterns = [
-    url(r'^$', views.expense_list),
+    url(r'^$', views.expense_list, name="list"),
     url(r'^month/([0-9]{4})/$', views.expense_list),
     url(r'^month/all/(?P<month>[0-9]{1,2})/$', views.expense_list, kwargs={'year':None}),
     url(r'^month/([0-9]{4})/([0-9]{1,2})/$', views.expense_list),
 
-    url(r'^([0-9]+)/$', views.expense_detail),
+    url(r'^detail/([0-9]+)/$', views.expense_detail,
+        name="detail"),
 
 ]

51ce0d40402d2ec6ac752b7e24efb509665f04c2
Udi Oron <udioron@gmail.com>
2017-03-23 11:43:26 +0200

    Added Categories

--- core/management/commands/create_expenses.py
+++ core/management/commands/create_expenses.py
@@ -4,7 +4,8 @@ import random
 import silly
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
-from django.db import IntegrityError
+from django.db import IntegrityError, transaction
+from django.utils.text import slugify
 
 from core.models import Expense
 
@@ -33,23 +34,35 @@ class Command(BaseCommand):
 
         users = list(User.objects.all())
 
-        for i in range(options['n']):
-            o = Expense(
-                user=random.choice(users),
-                date=datetime.date(
-                    random.randint(2010, 2017),
-                    random.randint(1, 12),
-                    random.randint(1, 28)
-                ),
-                title=silly.a_thing(),
-                amount=random.randint(1, 1000),
-                description=paragraph(random.randint(2, 4)),
-            )
-            o.full_clean()
-            o.save()
-
-            for j in range(random.randint(3, 10)):
-                o.comments.create(
-                    user=random.choice(users),
-                    content=paragraph(random.randint(1, 6))
+        for u in users:
+            while u.categories.count() < 4:
+                s = silly.a_thing().title()  # "Beautiful cat"
+                u.categories.create(
+                    title=s,
+                    slug=slugify(s),
                 )
+
+        for i in range(options['n']):
+            with transaction.atomic():
+                o = Expense(
+                    user=random.choice(users),
+                    date=datetime.date(
+                        random.randint(2010, 2017),
+                        random.randint(1, 12),
+                        random.randint(1, 28)
+                    ),
+                    title=silly.a_thing(),
+                    amount=random.randint(1, 1000),
+                    description=paragraph(random.randint(2, 4)),
+                )
+                o.full_clean()
+                o.save()
+
+                o.categories = random.sample(
+                    list(o.user.categories.all()), random.randint(0, 2))
+
+                for j in range(random.randint(3, 10)):
+                    o.comments.create(
+                        user=random.choice(users),
+                        content=paragraph(random.randint(1, 6)),
+                    )
--- core/models.py
+++ core/models.py
@@ -1,17 +1,34 @@
+from django.conf import settings
 from django.db import models
 from django.urls import reverse
-from django.conf import settings
+
+
+class Category(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             related_name='categories',
+                             on_delete=models.CASCADE)
+    title = models.CharField(max_length=300)  # "Food!!!"
+    slug = models.CharField(
+        max_length=300)  # "food" for /expense/category/food/
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
 
 
 class Expense(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              related_name='expenses',
                              on_delete=models.CASCADE)
-    date = models.DateField()
+    date = models.DateField(db_index=True)
     amount = models.DecimalField(decimal_places=2,
                                  max_digits=10)
     title = models.CharField(max_length=300)
     description = models.TextField(null=True, blank=True)
+
+    categories = models.ManyToManyField(
+        Category, related_name='expenses', blank=True,
+    )
 
     def __str__(self):
         return "[#{}] {} {:,}@{}".format(
@@ -23,7 +40,6 @@ class Expense(models.Model):
 
     def get_absolute_url(self):
         return reverse("expenses:detail", args=(self.id,))
-
 
 
 class Comment(models.Model):
